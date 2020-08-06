c778fc6fb24a852b4a947546d4a8cb5c10897d11
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 02:24:39 -0700

    Added project routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
+      resources :projects, only: [:index, :show]
     end
   end
 end
