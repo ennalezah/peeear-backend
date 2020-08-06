4ecb030e5a4cdc6d9b867a3b9c840337b0676f16
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 02:48:44 -0700

    Added index and show actions to projects controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::ProjectsController < ApplicationController
+
+  def index
+    projects = Project.all
+    render json: projects
+  end
+
+  def show
+    project = Project.find_by(id: params[:id])
+    render json: project
+  end
 end
