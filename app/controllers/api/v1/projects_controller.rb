c9c28c07c09354c74dd1ad3327654f5667d7faf3
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 03:01:01 -0700

    Added serializer to projects controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,11 +2,16 @@ class Api::V1::ProjectsController < ApplicationController
 
   def index
     projects = Project.all
-    render json: projects
+    render json: ProjectSerializer.new(projects).to_serialized_json, status: :accepted
   end
 
   def show
     project = Project.find_by(id: params[:id])
-    render json: project
+
+    if project
+      render json: ProjectSerializer.new(project).to_serialized_json, status: :accepted
+    else
+      render json: "Project not found. Check project ID and try again.", status: :not_found
+    end
   end
 end
