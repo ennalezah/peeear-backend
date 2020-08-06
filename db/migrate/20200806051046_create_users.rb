8bb0a36ca056cfabb80e2396ad30a10541205c24
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-05 22:19:06 -0700

    Ran user migration file

--- db/migrate/20200806051046_create_users.rb
+++ db/migrate/20200806051046_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :first_name,
-      t.string :last_name,
-      t.string :email,
-      t.string :password,
-      t.string :profile
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.string :profile_url
 
       t.timestamps
     end
