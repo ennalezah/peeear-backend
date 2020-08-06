6b5553a07bef3119ff870d17c1fc7317df5b3193
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 01:19:20 -0700

    Fixed spelling error in show action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    user = User.find_by(is: params[:id])
+    user = User.find_by(id: params[:id])
     render json: user
   end
 end
