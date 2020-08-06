83f4fce4d0ce16ef96716b43ad223f4a564596a7
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 01:03:31 -0700

    Created user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  first_name: 'Daphne',
+  last_name: 'Blue',
+  email: 'daphneb@yahoo.com',
+  password: 'password',
+  profile_url: 'daphneblue.com'
+)
+
+User.create(
+  first_name: 'Hazel',
+  last_name: 'Villareal',
+  email: 'hazelv@gmail.com',
+  password: 'password',
+  profile_url: 'github.com/ennalezah'
+)
