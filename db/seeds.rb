08cd2c85bef31a625dc6d544e69220e519a3af41
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 02:56:11 -0700

    Created project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,15 @@ User.create(
   password: 'password',
   profile_url: 'github.com/ennalezah'
 )
+
+Project.create(
+  title: 'To-do App',
+  description: 'Looking to create a simple to-do list app where user can add and delete items.',
+  difficulty: 'Beginner'
+)
+
+Project.create(
+  title: 'Time Me',
+  description: 'Need help with my timer project. User should be able to add a time, start, stop, and restart timer.',
+  difficulty: 'Beginner'
+)
