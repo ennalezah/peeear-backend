4c4ffd8c31463922245e61351ecd0151da7f1262
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-06 23:21:46 -0700

    Ran comment migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_090420) do
+ActiveRecord::Schema.define(version: 2020_08_07_060341) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "input"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
