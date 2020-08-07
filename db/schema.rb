7e2e9882234efd6b1bdbdd9229a6398441105474
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-07 03:22:51 -0700

    Added user_id column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_101306) do
+ActiveRecord::Schema.define(version: 2020_08_07_102141) do
 
   create_table "comments", force: :cascade do |t|
     t.text "input"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_101306) do
     t.string "difficulty"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
