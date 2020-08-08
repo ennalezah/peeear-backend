8da1062ef4d3b0ff130185b05c455364b5e5f9b7
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-07 23:27:50 -0700

    Created index and create actions in comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::CommentsController < ApplicationController
 
+  def index
+    comments = Comment.all
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      render json: comment
+    else
+      render json: {errors: comment.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:input, :user_id, :project_id)
+  end
+
 end
