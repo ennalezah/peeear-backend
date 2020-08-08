9f077cb56c459a831ec6ac75b7cd50c7e7fc169f
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-08 01:03:25 -0700

    Added destroy action in comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,17 +1,22 @@
 class Api::V1::CommentsController < ApplicationController
-
   def index
     comments = Comment.all
+    render json: comments
   end
 
   def create
     comment = Comment.new(comment_params)
 
     if comment.save
-      render json: comment
+      render json: comment, status: 200
     else
-      render json: {errors: comment.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: "There was a problem saving the comment."}, status: :unprocessible_entity
     end
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id])
+    comment.destroy
   end
 
   private
