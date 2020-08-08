71606d344f5a4bd1ffa6e83707c3ccc9e62af2fe
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-07 23:38:05 -0700

    Added routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show]
       resources :projects, only: [:index, :show]
+      resources :comments, only: [:index, :create]
     end
   end
 end
