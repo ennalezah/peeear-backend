5ef8c3aa45268ff4d306aec65ef237fcb9829cfc
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-09-04 04:46:16 -0700

    Added default scope order to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   # belongs_to :owner, class_name: "User"
-  
   # has_many :comments
   # has_many :users, through: :comments
+
+  default_scope { order(created_at: :desc) }
 end
